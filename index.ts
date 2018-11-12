33f0e6dd8f627819146e67cd425771f313ad544b
Shane Osbourne <shane.osbourne8@gmail.com>
2018-11-12 21:12:24 +0000

    comments/readme

--- index.ts
+++ index.ts
@@ -1,16 +1,26 @@
-type LocalReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
-type EventTypeMap<T extends { [key: string]: {} }> =
-    {
-        [K in keyof T]: {
-        type: K;
-        payload: LocalReturnType<T[K]>
-    }
-    };
-
-export type ActionHandler<T extends { [key: string]: {} }> =
-    EventTypeMap<T>[keyof EventTypeMap<T>]
-
-export function msgCreator<Obj extends { [key: string]: (...args: any[]) => any }>(input: Obj) {
+/**
+ * Accept a plain object literal where the keys are stings (or enum members)
+ * and produce a function that can be called with 100% typesafety.
+ *
+ * This is done by using Typescripts ability to infer the return types of functions
+ * combined with the ability to create new types from existing ones (mapped types)
+ *
+ * eg:
+ *
+ * const messages = {
+ *      SignedIn: (firstname: string, lastname: string) => ({firstname, lastname}),
+ *      Token: (token: string) => token,
+ *      SignOut: () => undefined,
+ * };
+ *
+ * export const Msg = msgCreator(messages);
+ *
+ * Msg("Token", "12345") // OK
+ * Msg("token", "12345") // error
+ * Msg("Token", 12345)   // error
+ *
+ */
+export function msgCreator<Obj extends DefaultMessageDefinition>(input: Obj) {
     return function Msg<Kind extends string>(kind: Kind, ...args: Parameters<typeof input[Kind]>) {
         const output = input[kind].apply(null, args);
         if (output === undefined) {
@@ -24,3 +34,49 @@ export function msgCreator<Obj extends { [key: string]: (...args: any[]) => any 
 }
 
 export default msgCreator;
+
+/**
+ * Defines the structure of the Object literal
+ * that's used to create all the inferred types.
+ *
+ * eg:
+ *
+ * const messages = {
+ *   Increment: (num = 1) => num
+ * }
+ *
+ */
+type DefaultMessageDefinition = {
+    [key: string]: (...args: any[]) => any
+};
+
+/**
+ * Use this to create a union of all possible action
+ * types in the form {type: string, payload: inferred}.
+ *
+ * You can then use this union type with something
+ * like a switch statement to narrow the type-checking
+ *
+ * Eg:
+ *
+ *   type AllActions = ActionHandler<typeof messages>
+ */
+export type ActionHandler<T extends { [key: string]: {} }> = ActionTypeMap<T>[keyof ActionTypeMap<T>]
+
+/**
+ * Converts the `messages` structure to type + payload, where type
+ * is the original Object key, and 'payload' is the inferred return type
+ */
+type ActionTypeMap<T extends { [key: string]: {} }> = {
+    [K in keyof T]: {
+        type: K;
+        payload: LocalReturnType<T[K]>
+    }
+};
+
+/**
+ * This type will extract the inferred return type of a
+ * given function (T)
+ */
+type LocalReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
+
